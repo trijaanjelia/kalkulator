f56967af652d89ed0ce49c3dd9a111270b92600d
trijaanjelia <trijaanjelia03@gmail.com>
2026-04-14 15:02:46 +0800

    Menambahkan fitur scientific (sin cos tan)

--- script.js
+++ script.js
@@ -8,14 +8,30 @@ function clearDisplay() {
     display.value = "";
 }
 
-function deleteLast() {
-    display.value = display.value.slice(0, -1);
-}
-
 function hitung() {
     try {
         display.value = eval(display.value);
     } catch {
         display.value = "Error";
     }
+}
+
+// 🔥 TRIGONOMETRI (DERAJAT)
+function toRadians(deg) {
+    return deg * (Math.PI / 180);
+}
+
+function sin() {
+    let value = parseFloat(display.value);
+    display.value = Math.sin(toRadians(value)).toFixed(4);
+}
+
+function cos() {
+    let value = parseFloat(display.value);
+    display.value = Math.cos(toRadians(value)).toFixed(4);
+}
+
+function tan() {
+    let value = parseFloat(display.value);
+    display.value = Math.tan(toRadians(value)).toFixed(4);
 }
